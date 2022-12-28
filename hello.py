ab0a1938ae5d90fe3561a32e57ca487fa0e5b30a
Alexandra Efremova <aap1992@yandex.ru>
2022-12-28 22:38:56 +0300

    add submit

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world!"
-
+    return """
+    <html>
+        <head>
+            <title>Витамины и БАДы NOW</title>
+        </head>
+            <body>
+                <h1>Введите название и дозировку</h1>
+                    <h2>
+                        <form action="/whatev" method="get">
+                        <input type="text" name="hello" value="">
+                        <input type="submit" value="Поиск">
+                        </form>
+                    </h2>
+            </body>
+    </html>
+"""
+    
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
