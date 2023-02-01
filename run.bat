bf62fc6a1ca3d7a73cb26958d65a7a2027576f02
Alexandra Efremova <aap1992@yandex.ru>
2023-02-01 22:13:37 +0300

    _

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask save-productswb
+set FLASK_APP=webapp && set FLASK_ENV=development && set FLASK_DEBUG=1 && flask save-products
